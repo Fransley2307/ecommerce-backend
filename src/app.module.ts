46f0e726aea0ccfc69791291d6ecfd7f0a8b6ede
Fransley Brustolim Ferreira <fransley0@gmail.com>
2025-08-18 22:04:00 -0300

    Aula18/08/2025
    Finalizado Implementação do Brend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './cases/categories/category.module';
+import { BrandModule } from './cases/brands/brand.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'aws-0-sa-east-1.pooler.supabase.com',
+      host: 'aws-1-sa-east-1.pooler.supabase.com',
       port: +'5432',
-      username: 'postgres.omysealyawzquilsphgw',
+      username: 'postgres.tcgxsxunwvjhjdbljepc',
       password: '152776',
       database: 'postgres',
-    })
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CategoryModule,
+    BrandModule
   ],
 })
 export class AppModule {}
